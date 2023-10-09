6eaf2b371f02a9c95352f7352d44b6f63c3288a0
house <housesellout@gmail.com>
2023-10-09 17:42:30 -0400

    :- public maria chat update

---
+++ maria/maria_irc_100923_2.py
@@ -0,0 +1,192 @@
+str_alt = '''
+[3:31pm] PassportPowell: Not interested in chasing green candles that have ran up that much that quickly. plus you've given us an idea of what may be good token to look into (nfa)
+[3:31pm] sayyid: hahaha
+[3:31pm] Higher left the chat room. (Remote host closed the connection)
+[3:31pm] thecyclewatcher joined the chat room.
+[3:31pm] hextrack left the chat room.
+[3:32pm] Tbill joined the chat room.
+[3:32pm] moose_: @mariarahel what do you mean when you say there are certain price floors that ATROPA can't go under. Does that remain true if PLS goes down past a certain level?
+[3:33pm] ead11: Pls won't drop much further or its already on its way to rebound
+[3:33pm] sayyid: any ideas why u created this ਓੰਫਃਙ਼ੌਘਁ?
+[3:33pm] Tbill left the chat room. (Remote host closed the connection)
+[3:34pm] thecyclewatcher: this irc chat thing makes me feel earlhy
+[3:34pm] ead11: Expecting a pre pump before sec date as people will already have fomo if case closes positivity and they don't got time to enter
+[3:34pm] SwissAlpen1 joined the chat room.
+[3:34pm] moose_: Yeah I'm just curious if it abides by its own functions and whether or not the PLS price can alter the behaviour of the bots
+[3:35pm] Assie left the chat room. (Remote host closed the connection)
+[3:38pm] PVibrations left the chat room. (Remote host closed the connection)
+[3:38pm] Naming joined the chat room.
+[3:40pm] darkangelsimo_ joined the chat room.
+[3:41pm] Mav_ joined the chat room.
+[3:41pm] db_: maria, can you added some lp for IRC and let community push this.
+[3:41pm] Naming: Do you have any info on the payment done to makerDao before minting on pdai changed?
+[3:42pm] moose_: ^
+[3:42pm] PassportPowell: I asked this earlier but i dont think i receive a response
+[3:42pm] moose_: yeah I don't think we're gonna get one tbh, the txn was made on Tornado Cash for a reason
+[3:42pm] vit joined the chat room.
+[3:43pm] PassportPowell: exactly
+[3:43pm] PassportPowell: What I am thinking though is that we can as mariarahel questions about these things but it's the way we're asking maybe...
+[3:43pm] PassportPowell: how vs what
+[3:43pm] MichaelT joined the chat room.
+[3:44pm] JR left the chat room. (Ping timeout: 480 seconds)
+[3:45pm] xCETiSx left the chat room. (Quit: Page closed)
+[3:47pm] PKNDGG joined the chat room.
+[3:47pm] MikeFA: mariarahel will you launch a love token to balance the war token? (or will atropa 414?)
+[3:47pm] futureWhale_ left the chat room. (Remote host closed the connection)
+[3:49pm] pedrodelavega: Is there any chance that 2cc could be as successful as CIA?
+[3:49pm] Mav_ left the chat room. (Ping timeout: 480 seconds)
+[3:49pm] SG: Hello @mariarahel, I just want to thankyou for your time and hardwork, it is mind boggling,, Before you leave this chat because of every peasant looking for gains
+[3:49pm] MikeFA: +
+[3:51pm] db_: thanks maria. I think I'll thank to IRC token. because it was met us here. and I will try to push it as a sign of my thanks
+[3:51pm] SG: I'm so excited to be here and understand the concepts
+[3:54pm] SAYYID__ joined the chat room.
+[3:54pm] PassportPowell: mariarahel Do you tihnk there will be any us cases for the $INC token?
+[3:54pm] DGGPKN left the chat room. (Ping timeout: 480 seconds)
+[3:54pm] PassportPowell: Also are considering using it with the 414 tokens?
+[3:55pm] JR joined the chat room.
+[3:56pm] MichaelT left the chat room. (Ping timeout: 480 seconds)
+[3:57pm] SAYYID__: yo guys i make community tokens group telegram too for ޖޮޔިސްދޭވޯހީ = Joycedevohi and Hegemony - عجթ. will welcome of you guys be a part of this @Joycedevohi and @hegemony_prc20.
+[3:57pm] udin: Count me in
+[3:58pm] chapweezy joined the chat room.
+[3:59pm] vit left the chat room. (Quit: Page closed)
+[3:59pm] pulseperza joined the chat room.
+[3:59pm] Naming: The name atropa, is it connected with the series in your Playlist? The plant? Or atropos the balancing force as in Greek mythology?
+[4:00pm] Breeks joined the chat room.
+[4:00pm] Breeks left the chat room. (Remote host closed the connection)
+[4:00pm] Breeks joined the chat room.
+[4:01pm] Breeks left the chat room. (Remote host closed the connection)
+[4:01pm] SAYYID__ left the chat room. (Remote host closed the connection)
+[4:01pm] cro-magnon left the chat room. (Quit: Leaving)
+[4:03pm] JR left the chat room. (Remote host closed the connection)
+[4:03pm] PassportPowell: refreshment break lol
+[4:03pm] Breeks joined the chat room.
+[4:03pm] Breeks left the chat room. (Remote host closed the connection)
+[4:03pm] robin_: @nombramiento  Por como se está dando todo me declinó más  por atropo  la fuerza del equilibrio  de la mitología griega
+[4:11pm] Tbill joined the chat room.
+[4:12pm] OCG joined the chat room.
+[4:13pm] OCG left the chat room. (Remote host closed the connection)
+[4:14pm] OCG joined the chat room.
+[4:16pm] OCG left the chat room. (Remote host closed the connection)
+[4:16pm] pulseperza left the chat room. (Ping timeout: 480 seconds)
+[4:19pm] Tbill left the chat room. (Remote host closed the connection)
+[4:20pm] Assie joined the chat room.
+[4:22pm] MikeFA: Rahel Varnhagen was Jewish and very belight. Can her live provide insights about current events?
+[4:23pm] mariarahel set a ban on *!*oftc-webi@37.19.210.10.
+[4:24pm] PassportPowell: good ban
+[4:24pm] sayyid: nice
+[4:25pm] JR joined the chat room.
+[4:25pm] thecyclewatcher left the chat room. (Remote host closed the connection)
+[4:25pm] grateful4today left the chat room. (Remote host closed the connection)
+[4:26pm] Suave left the chat room. (Read error: Connection reset by peer)
+[4:26pm] robin_: See🤘
+[4:26pm] Suave joined the chat room.
+[4:27pm] JR left the chat room. (Remote host closed the connection)
+[4:29pm] Mav_ joined the chat room.
+[4:30pm] RedCandleChaser left the chat room. (Quit: Page closed)
+[4:33pm] RazoR joined the chat room.
+[4:33pm] PassportPowell left the chat room. (Ping timeout: 480 seconds)
+[4:33pm] StacyKalashnikov: is everyone silent?
+[4:33pm] sayyid: working on liquidity😀
+[4:33pm] PassportPowell joined the chat room.
+[4:34pm] mariarahel: liquidity war
+[4:34pm] Skye joined the chat room.
+[4:35pm] FREEDOM_Tokens777: LFG
+[4:36pm] StacyKalashnikov: who’s winning *cutiesmile*
+[4:37pm] moose_: Is Japanese Teddy Bear also a memecoin?
+[4:37pm] Mav_ left the chat room. (Ping timeout: 480 seconds)
+[4:38pm] mariarahel: it is a financial instrument, it has already been used a few times
+[4:38pm] StacyKalashnikov: what is FROCK?
+[4:38pm] moose_: Interesting
+[4:38pm] PassportPowell: Who was the liquidity war message in response to?
+[4:39pm] sayyid: my chat
+[4:39pm] FREEDOM_Tokens777: Maria stirring up the Battlefield. Will be interesting to see how all of these 414 Liquidity pairs evolve with PLS and other 414 pairings.
+[4:40pm] RazoR left the chat room. (Remote host closed the connection)
+[4:40pm] PassportPowell: Mariarahel what do you think about an lp between inc and atropa?
+[4:40pm] moose_: why was the NoNukes token the only one that was paired with INC rather than wPLS
+[4:41pm] mariarahel: incentive to me suggests a domain of behaviors
+[4:41pm] PassportPowell: can you elaborate on "domain of behaviors"?
+[4:42pm] FREEDOM_Tokens777: Nice!
+[4:42pm] RazoR joined the chat room.
+[4:43pm] Grav: maria are you using a translator? and I'll be the first to ask if you're Ai.
+[4:44pm] sayyid:  "any ideas why u created this ਓੰਫਃਙ਼ੌਘਁ?" still waiting this answer maria, let me know
+[4:46pm] moose_: Since the dOWN token acts as the "savings account" of the ecosystem, is this the token that ensures maintains price floors?
+[4:47pm] breeks joined the chat room.
+[4:47pm] PassportPowell: Do you mean as in to influence people to do certain things using the tokens/lp ?
+[4:47pm] RazoR left the chat room. (Remote host closed the connection)
+[4:48pm] SwissAlpen1 left the chat room. (Quit: Leaving)
+[4:49pm] PassportPowell: or behaviour associated with  greed?
+[4:49pm] claudia_ joined the chat room.
+[4:49pm] b0s: 1 token i want to know what its intention is, is $Burr
+[4:49pm] _Tbill_OnX joined the chat room.
+[4:50pm] _Tbill_OnX: mariarahel:
+[4:50pm] _Tbill_OnX: You there?
+[4:50pm] FREEDOM_Tokens777: Liquidity Providing is Incentivized
+[4:50pm] evelina_: What about ᨓᨆ? What is it for?
+[4:50pm] moose_: ^
+[4:51pm] PassportPowell: I assume when maria doesn't reply it's because 1.the questions are bad, 2. doxxing, 3. they're busy
+[4:51pm] PassportPowell: lol
+[4:51pm] FREEDOM_Tokens777: Probably.
+[4:51pm] b0s: maria will atropa 1000x
+[4:52pm] PassportPowell: Liquidity Providing is Incentivized. Sorry i missed your point on this. Yeah lp is incentivised but apart from that i'm lost
+[4:52pm] moose_: well we've asked what the significance of other tokens are and maria has answered so I think those questions are fine
+[4:52pm] moose_: prob afk
+[4:57pm] evelina_: Can someone explain ᨓᨆ? A money printer? What was that?
+[4:57pm] b0s: did any of you watch the youtube video in the "Topic" ...
+[4:58pm] _Tbill_OnX left the chat room. (Ping timeout: 480 seconds)
+[4:59pm] FREEDOM_Tokens777: Token A + Token B = Token C. Lol https://usercontent.irccloud-cdn.com/file/ALLlDhe1/Liquidity.jpg
+[4:59pm] Suave left the chat room. (Ping timeout: 480 seconds)
+[5:00pm] db_: what is that
+[5:00pm] sayyid: its forks ERC20 too PRC20 like pDai
+[5:00pm] FREEDOM_Tokens777: It's a video of Richard Heart talking a little bit about Liquidity.
+[5:02pm] moose_: where is token C there? Its just ERC-PRC LP pairing
+[5:02pm] FREEDOM_Tokens777: When you put token A together with Token B, you get Token C 👍
+[5:03pm] Grav: He mentioned it on the stream during the Miami WorldCryptoCon.
+[5:03pm] moose_: so when you pair Token A: wBTC with Token B: pWBTC what token do you get?
+[5:03pm] Grav: You can find it on hexsearch.io
+[5:03pm] Grav: "bridging in erc20's"
+[5:03pm] FREEDOM_Tokens777: moose_: Bitcoin money printer.
+[5:04pm] b0s: but can atropa hit $5
+[5:04pm] moose_: whats the Token C ticker lol?
+[5:04pm] FREEDOM_Tokens777: moose_: PLP!!! lol.
+[5:04pm] FREEDOM_Tokens777: Your Liquidity Token!
+[5:05pm] moose_: ah
+[5:05pm] Grav: plp to dah mooon!
+[5:05pm] JR_ joined the chat room.
+[5:06pm] FREEDOM_Tokens777: One of my favorite tokens, hahaha.
+[5:06pm] PassportPowell: https://www.youtube.com/watch?v=11HuXKwL-N8 I must be missing something because i have a basic idea of lp providing and bridging lol
+[5:08pm] JR_ left the chat room. (Remote host closed the connection)
+[5:10pm] FREEDOM_Tokens777: That's good 👍 you are incentivized to provide Liquidity on Pulsechain and maybe pulsechain will be a really big awesome epic blockchain one day you you got a tiny little piece of willy wonkas chocolate factory 👍
+[5:11pm] PassportPowell: lol ok. But I wanted to know marias thoughts on INC and lp with it
+[5:11pm] Viking joined the chat room.
+[5:11pm] PassportPowell: now were talking about willy wonka
+[5:14pm] db_: why willy wonka?
+[5:14pm] FREEDOM_Tokens777: Liquidity is very important .
+[5:14pm] PassportPowell: because when you lp you get some of the factory
+[5:14pm] FREEDOM_Tokens777: Yes it was just a joke but I was being serious at the same time.
+[5:15pm] PassportPowell: yeah i got it
+[5:15pm] db_: copy that
+[5:19pm] chapweezy: Has maria chatted yet today?
+[5:20pm] chapweezy: Scratch that, I see a message from not long ago
+[5:20pm] PKNDGG left the chat room. (Quit: Leaving)
+[5:21pm] DGGPKN joined the chat room.
+[5:23pm] Tetragrammaton666 left the chat room. (Quit: This computer has gone to sleep)
+[5:23pm] cro-magnon joined the chat room.
+[5:24pm] DGGPKN left the chat room.
+[5:25pm] Tetragrammaton joined the chat room.
+[5:26pm] DTA73 joined the chat room.
+[5:27pm] Naming left the chat room. (Remote host closed the connection)
+[5:27pm] RazoR joined the chat room.
+[5:29pm] RazoR left the chat room.
+[5:32pm] _Tbill_OnX joined the chat room.
+[5:32pm] _Tbill_OnX left the chat room.
+[5:34pm] pdizzle3:  Mariarahel where would you like to see people provide liquidity inside your ecosystem? Atropa pairs? pls pairs? Paring up of new coins as they are released?
+[5:35pm] Tetragrammaton: What'd i miss? ?
+
+    '''
+
+lines = str_alt.split('\n')
+#filtered_lines = [line +'\n' for line in lines if 'mariarahel' in line]
+filtered_lines = [line +'\n' for line in lines if 'maria' in line]
+for l in filtered_lines:
+    print(l)
+
